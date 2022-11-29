1c087c6514807b877fda75e84e359ce7a6ed91bf
Reyhan Firdaus <rey130499@gmail.com>
2022-11-29 17:16:04 +0700

    ptint()

--- detect.py
+++ detect.py
@@ -156,6 +156,7 @@ def run(
                 for c in det[:, -1].unique():
                     n = (det[:, -1] == c).sum()  # detections per class
                     s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+                    print(n)
 
                 # Write results
                 for *xyxy, conf, cls in reversed(det):
