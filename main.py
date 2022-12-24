32e7228e7d5735edc8211c517a7f926d4e29003c
Sahrial Ihsani Ishak <sahrialishak@gmail.com>
2022-12-24 14:17:57 +0700

    c

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ from io import StringIO
 from pathlib import Path
 import streamlit as st
 import time
-import detect_image
 from detect_image import main
 import os
 import sys
 import argparse
 from PIL import Image
-import cv2
 import streamlit as st
 from deep_list import *
 import torch
