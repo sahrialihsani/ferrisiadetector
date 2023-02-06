cb47c7491cd3dd97615e82b452026d0b79b21e1e
Sahrial Ihsani Ishak <sahrialishak@gmail.com>
2023-02-06 11:45:12 +0700

    commit

--- detect_image.py
+++ detect_image.py
@@ -202,7 +202,7 @@ def run(
                         windows.append(p)
                         cv2.namedWindow(str(p), cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)  # allow window resize (Linux)
                         cv2.resizeWindow(str(p), im0.shape[1], im0.shape[0])
-                    cv2.imshow(str(p), im0)
+                    # cv2.imshow(str(p), im0)
                     cv2.waitKey(1)  # 1 millisecond
                     cv2.imwrite(save_path, im0)
 
